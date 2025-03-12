f8e9a97022f5aea826d9759aab9cfc27611d78ec
Reem-O-Saleh <123761729+Reem-O-Saleh@users.noreply.github.com>
2025-03-12 19:45:13 +0000

    finished

--- congress.py
+++ congress.py
@@ -1,22 +1,42 @@
 import os
 import json
+from datetime import datetime 
 import requests
+from slack import WebClient
+from slack.errors import SlackApiError
 
+slack_token = os.environ.get('SLACK_API_TOKEN')
 congress_api_key = "tvT7WNJdrx0ELB1T5ccyjpyJpWwtgjDmSV8Tywwm"
 
-url = f"https://api.congress.gov/v3/committee-report/119/hrpt?api_key={congress_api_key}&format=json"
 
-print(url)
+url = f"https://api.congress.gov/v3/committee-report/119/hrpt?api_key={congress_api_key}&format=json"
 
 r = requests.get(url)
 
 results = r.json()
 
-fr_url = results['reports'][0]['url']
+first_result = results['reports'][0]
 
-fr = requests.get(fr_url + f"&api_key={congress_api_key}")
+display_url = f"https://www.congress.gov/congressional-report/{first_result['congress']}th-congress/house-report/{first_result['number']}"
 
-result = fr.json()
+display_date = datetime.strptime(first_result['updateDate'], '%Y-%m-%dT%H:%M:%SZ')
+formatted_date = display_date.strftime('%B %-d, %Y at %-I:%M%p')
+sentence = f"On {formatted_date}, the House published {first_result['citation']}, which is available at {display_url}"
 
-print(result)
+print(sentence)
 
+
+client = WebClient(token=slack_token)
+msg = sentence 
+try:
+    response = client.chat_postMessage(
+        channel="slack-bots",
+        text=msg,
+        unfurl_links=True, 
+        unfurl_media=True
+    )
+    print("success!")
+except SlackApiError as e:
+    assert e.response["ok"] is False
+    assert e.response["error"]
+    print(f"Got an error: {e.response['error']}")
